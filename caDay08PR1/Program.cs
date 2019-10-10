2930386fe6f198465c91f9e85b1a80857d75a206
Muhammad Akbar <akbarprima95@gmail.com>
2019-10-10 20:54:17 +0700

    soal batch215 nomor 1

---
+++ caDay08PR1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caDay08PR1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NOMOR 1 SOAL BATCH215
+            Console.Write("Masukkan nilai mahasiswa: ");
+            int[] nilai = Array.ConvertAll(Console.ReadLine().Split(' '), x => Convert.ToInt32(x));
+            int range = nilai.Length;
+            int tmpg = 0;
+
+            Console.WriteLine();
+            int nilaiMax = nilai.Max();
+            for (int i = range-1; i >= 0; i--)
+            {
+                if (nilaiMax == nilai[i])
+                {
+                    tmpg = tmpg + 1;
+                }
+            }
+            Console.WriteLine("Nilai tertingginya yaitu {0}", nilaiMax);
+            Console.Write("Ada {0} mahasiswa", tmpg);
+            Console.ReadKey();
+        }
+    }
+}
