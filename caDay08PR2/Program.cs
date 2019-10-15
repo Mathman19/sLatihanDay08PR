7d386e9ce1c2997cf82f065b1a42388fea9bc5cb
Muhammad Akbar <akbarprima95@gmail.com>
2019-10-15 19:28:59 +0700

    Soal batch215 nomor 2

---
+++ caDay08PR2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caDay08PR2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan angka berapapun : ");
+            int angka = int.Parse(Console.ReadLine());
+            int a = 0;
+
+            for (int i = 1; i <= angka; i++)
+            {
+                if (angka % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine(angka + " termasuk angka prima");
+            }
+            else
+            {
+                Console.WriteLine(angka + " bukan angka prima");
+            }
+            Console.ReadKey();
+        }
+    }
+}
