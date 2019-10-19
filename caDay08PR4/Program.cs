87802d89ca2460b0d4b3f6011caaba32ae1c8c62
Muhammad Akbar <akbarprima95@gmail.com>
2019-10-19 15:47:30 +0700

    soal batch 215 nomor 4

---
+++ caDay08PR4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caDay08PR4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int angka = int.Parse(Console.ReadLine());
+            int range = 10 - angka;
+            int tmpg = 0;
+
+            for (int i = 1; i <= range; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    if (i < j)
+                    {
+                        tmpg++;
+                    }
+                }
+            }
+            Console.WriteLine(tmpg);
+            Console.ReadKey();
+        }
+    }
+}
