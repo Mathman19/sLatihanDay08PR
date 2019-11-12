8bdd2f0b033135354e0dee28d33e0f64d73e4394
Muhammad Akbar Primasaputra <54655878+Mathman19@users.noreply.github.com>
2019-11-12 11:23:16 +0700

    soal batch215 nomor 9

---
+++ caDay08PR9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caDay08PR9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Rata-rata awal ");
+            int meanAwal = int.Parse(Console.ReadLine());
+            Console.Write("Rata-rata akhir ");
+            int meanAkhir = int.Parse(Console.ReadLine());
+            Console.Write("Ada tambahan berapa Anak? ");
+            int banyakAnak = int.Parse(Console.ReadLine());
+            Console.Write("Nilai anak tersebut? ");
+            int nilaiAnak = int.Parse(Console.ReadLine());
+            int jumlahKelas = banyakAnak * (nilaiAnak - meanAkhir) / (meanAkhir - meanAwal);
+            Console.WriteLine(jumlahKelas);
+            Console.ReadKey();
+        }
+    }
+}
