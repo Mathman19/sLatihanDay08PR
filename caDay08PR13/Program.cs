3cdb1d3d7880f7c1f72caa34219a764cc19b7bea
Muhammad Akbar Primasaputra <54655878+Mathman19@users.noreply.github.com>
2019-11-26 14:00:57 +0700

    soal batch 215 nomor 13

---
+++ caDay08PR13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caDay08PR13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tmp;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i<i+1)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
